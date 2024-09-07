2d2b75bbbb256d0d016a282e07bc1024e2734061
MikolajMarciniak <marciniak.miko@gmail.com>
2024-09-07 13:31:08 +0100

    configere deply script for AWS profiles

--- deploy.js
+++ deploy.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const { execSync } = require('child_process');
 
 const bucketName = process.env.AWS_BUCKET_NAME;
+const profileName = process.env.AWS_PROFILE_NAME;
 
 if (!bucketName) {
   console.error("AWS_BUCKET_NAME is not defined in the environment variables.");
@@ -10,7 +11,7 @@ if (!bucketName) {
 }
 
 // Run the AWS CLI command
-const command = `aws s3 sync ./build s3://${bucketName}/`;
+const command = `aws s3 sync ./build s3://${bucketName}/ --profile ${profileName}`;
 
 try {
   execSync(command, { stdio: 'inherit' });
