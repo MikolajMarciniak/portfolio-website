88b565cbcd0efe44a3341c17b011a6e04c4dc157
MikolajMarciniak <marciniak.miko@gmail.com>
2024-09-18 16:23:17 +0100

    add breathing and pulse animation to spotlight

--- src/app/sections/About.js
+++ src/app/sections/About.js
@@ -4,8 +4,8 @@ const LandingSection = () => {
   return (
     <section className="landing-section min-h-screen flex items-center justify-center bg-gray-900 text-white">
       <div className="text-center">
-        <h1 className="text-4xl font-bold">Welcome to My Portfolio</h1>
-        <p className="mt-4 text-lg">Showcasing my work and projects.</p>
+        <h1 className="text-4xl font-bold">About me</h1>
+        <p className="mt-4 text-lg"></p>
       </div>
     </section>
   );
--- src/app/sections/Landing.js
+++ src/app/sections/Landing.js
@@ -4,8 +4,8 @@ const LandingSection = () => {
   return (
     <section className="landing-section min-h-screen flex items-center justify-center bg-gray-900 text-white">
       <div className="text-center shadow">
-        <h1 className="text-4xl font-bold">Welcome to My Portfolio</h1>
-        <p className="mt-4 text-lg">Showcasing my work and projects.</p>
+        <h1 className="text-7xl font-bold">Mikołaj Marciniak</h1>
+        <p className="text-4xl mt-4">Fullstack React Developer</p>
       </div>
     </section>
   );
