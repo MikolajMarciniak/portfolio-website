0778c74a4e486b03cf57be44c7d00852389c1d15
MikolajMarciniak <marciniak.miko@gmail.com>
2024-09-18 18:25:51 +0100

    update landing shadow with text typed

--- src/app/components/TypedText.js
+++ src/app/components/TypedText.js
@@ -1,29 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {ReactTyped} from 'react-typed';
 
-const coloredStrings = [
-  { text: "developer", color: "text-red-500" },
-  { text: "teacher", color: "text-blue-500" },
-  { text: "designer", color: "text-purple-500" },
-  { text: "freelancer", color: "text-yellow-500" },
-];
-
-const TypedText = () => {
-  const [colorClass, setColorClass] = useState(coloredStrings[0].color);
-
-  // Handle the color change based on the string being typed
-  const handleStringTyped = (index) => {
-    setColorClass(coloredStrings[index % coloredStrings.length].color);
-  };
-
+const TypedText = ({ strings, colorClass, handleStringTyped }) => {
   return (
     <span className={`inline-block ${colorClass}`}>
       <ReactTyped
-        strings={coloredStrings.map(s => s.text)}
+        strings={strings.map(s => s.text)}
         typeSpeed={40}
         backSpeed={50}
         loop
-        preStringTyped={handleStringTyped}
+        preStringTyped={index => handleStringTyped(index)}
       />
     </span>
   );
--- src/app/sections/Landing.js
+++ src/app/sections/Landing.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TypedText from '../components/TypedText';
 
+const coloredStrings = [
+  { text: "developer", color: "text-red-500", shadowColor: "#f87171" },
+  { text: "teacher", color: "text-blue-500", shadowColor: "#93c5fd" },
+  { text: "designer", color: "text-purple-500", shadowColor: "#d8b4fe" },
+  { text: "freelancer", color: "text-yellow-500", shadowColor: "#facc15" },
+];
+
 const LandingSection = () => {
+  const [colorClass, setColorClass] = useState(coloredStrings[0].color);
+  const [shadowColor, setShadowColor] = useState(coloredStrings[0].shadowColor);
+
+  const handleStringTyped = (index) => {
+    const currentString = coloredStrings[index % coloredStrings.length];
+    setColorClass(currentString.color);
+    setShadowColor(currentString.shadowColor);
+  };
+
   return (
     <section className="landing-section min-h-screen flex items-center justify-center bg-gray-900 text-white">
       <div className="text-center">
-        <h1 className="text-7xl font-bold">Hello, I'm <span className="shadow">Mikołaj Marciniak</span></h1>
+        <h1 className="text-7xl font-bold">
+          Hello, I'm <span className="shadow landing" style={{ "--shadow-color-landing": shadowColor }}>Mikołaj Marciniak</span>,
+        </h1>
         <div className="mt-6">
           <p className="text-4xl mt-4 text-white inline-block leading-none">
-            and I'm a &nbsp;<TypedText />
+            and I'm a <TypedText strings={coloredStrings} colorClass={colorClass} handleStringTyped={handleStringTyped} />
           </p>
         </div>
       </div>
