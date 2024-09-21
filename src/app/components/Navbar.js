34981ff46901ad76df5dd20bac47653dacf0e2d5
MikolajMarciniak <marciniak.miko@gmail.com>
2024-09-21 16:32:46 +0100

    fix navbar text-color

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = ({ toggleTheme, isDarkMode }) => {
             to="about"
             smooth={true}
             duration={500}
-            className="cursor-pointer relative group hover:text-indigo-500"
+            className="text-[var(--text-color)] cursor-pointer relative group hover:text-indigo-500"
           >
             About
             <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-indigo-500 transition-all duration-300 group-hover:w-full"></span>
@@ -46,7 +46,7 @@ const Navbar = ({ toggleTheme, isDarkMode }) => {
             to="projects"
             smooth={true}
             duration={500}
-            className="cursor-pointer relative group hover:text-indigo-500"
+            className="text-[var(--text-color)] cursor-pointer relative group hover:text-indigo-500"
           >
             Projects
             <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-indigo-500 transition-all duration-300 group-hover:w-full"></span>
@@ -55,7 +55,7 @@ const Navbar = ({ toggleTheme, isDarkMode }) => {
             to="contact"
             smooth={true}
             duration={500}
-            className="cursor-pointer relative group hover:text-indigo-500"
+            className="text-[var(--text-color)] cursor-pointer relative group hover:text-indigo-500"
           >
             Contact
             <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-indigo-500 transition-all duration-300 group-hover:w-full"></span>
@@ -74,7 +74,7 @@ const Navbar = ({ toggleTheme, isDarkMode }) => {
                 stroke="currentColor"
                 fill="currentColor"
                 stroke-width="0"
-                viewBox="-1 0 17 16"
+                viewBox="-1 0 19 16"
                 height="200px"
                 width="200px"
                 xmlns="http://www.w3.org/2000/svg"
@@ -82,11 +82,11 @@ const Navbar = ({ toggleTheme, isDarkMode }) => {
                 <path
                   d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278"
                   stroke="black"
-                  strokeWidth="0.75"
+                  strokeWidth="1"
                 ></path>
                 <path
                   stroke="black"
-                  strokeWidth="0.75"
+                  strokeWidth="1"
                   d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.73 1.73 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.73 1.73 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.73 1.73 0 0 0 1.097-1.097zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z"
                 ></path>
               </svg>
